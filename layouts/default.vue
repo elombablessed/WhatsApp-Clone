37fae1aadfd8b42186decd73d89fbf3638975053
Elomba Blessed <blessedelomba@gmail.com>
2020-09-08 15:11:16 +0100

    Adjusting the Apps Layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+    <div class="app">
+      <!-- This is the full App -->
+      
+      <Nuxt />
+    </div>
+
 </template>
 
 <style>
@@ -31,32 +34,10 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.app{
+display: grid;
+place-items: center;
+background-color: #dadbd3;
+height: 100vh;
 }
 </style>
